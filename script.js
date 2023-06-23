6cf31370e8f2e425ab56d18abd0fbbde9b817b6a
michellemwangi01 <michellemwangim@gmail.com>
2023-06-23 18:58:15 +0300

    Reset ButtonDone

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+ // <i class="fa-solid fa-heart fa-lg" style="color: #1f2123;"></i>
+//  <span class="stars"></span>
 function createCards(animal){
   const card = document.createElement('div')
                 document.querySelector('#mainContainer').append(card)
@@ -7,10 +8,13 @@ function createCards(animal){
             <div class="card ">
               <div class="face face1  ">
                 <div class="content">
-                  <span class="stars"></span>
+                <div class=cardHeader> 
+                  <h2 id='${animal.id}' class="votes">Votes: ${animal.votes}</h2>
+                  <button class="btnReset" id="btnReset-${animal.id}">Reset &#128546;</button>
+                </div>
+               
                   
-                  <h2 id='${animal.id}' class="votes">${animal.votes}</h2>
-                  <i class="fa-solid fa-heart fa-lg" style="color: #1f2123;"></i>
+                 
                   <img src="${animal.image}" class="cSharp"></img>
                 </div>
               </div>
@@ -30,6 +34,9 @@ function createCards(animal){
       card.addEventListener('mouseleave', (e)=>{
         cardFace.style.height = '100%'
       })      
+
+      let resetButton = document.getElementById(`btnReset-${animal.id}`)
+      resetButton.addEventListener('click',()=> resetVotesValue(animal))
     voteCounter(animal)
     return card
 }
@@ -65,6 +72,29 @@ function updateVotesValue(animal){
   .then((data)=> console.log(data))
 
   
+}
+
+function resetVotesValue(animal){
+  fetch(`http://localhost:3000/characters/${animal.id}`, {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(
+    {
+      "votes": 0
+    }
+  )
+  }) 
+  .then(res => {
+    if(res.ok){
+      console.log("SUCCESS")
+    }else{
+      console.log("UNSUCESSFULL")
+    }
+    return res.json()
+  })
+  .then((data)=> console.log(data))  
 }
 
 function voteCounter(animal){
